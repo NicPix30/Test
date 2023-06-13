6d226bfa0afdf06c60609a6a3c3cbb3b147dfad5
NicPix30 <117862776+NicPix30@users.noreply.github.com>
2023-06-13 05:48:21 -0500

    2 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var image = new Image();
-image.onload = function () {
-  context.drawImage(image, 0, 0, canvas.width, canvas.height);
-};
-image.src = '';
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Daisy {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+}
+
+let grid = [[], [], [], [], []];
+
+function BuyPlant() {
+  if (grid.length <= 5) {
+    grid.push(new Daisy('bob', `hsl(${Math.random() * 360}, 50%, 50%)`));
+  }
+}
+
+setInterval(function test() {
+  BuyPlant();
+  console.log(grid);
+}, 1000);
